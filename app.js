68043b4e9e6b960e1004f28c02b403eecafa1f7b
daxkongjh <dax.k@live.com>
2022-05-11 10:48:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { workerData } = require("worker_threads");
+
 
 // resources
 const $footer = $("<div>").addClass("footer");
@@ -20,6 +20,7 @@ const $missileButton = $("<button>")
 
 //### skills ####
 const battle = (event) => {
+ //   console.log("atkselector", atkselector)
   let execute = attack[atkselector];
   execute(event);
 };
@@ -63,23 +64,27 @@ const attack = {
 //### End Of Skills ####
 
 //Creating game characters
-const characters = {
+const hero = {
   ironman: {
     type: "hero",
     name: "ironman",
     level: 1,
-    speed: 11,
+    spd: 11,
     hp: 100,
     atk: 20,
     atkgrowth: 12,
     hpgrowth: 15,
     pic: $("<img>").attr("id", "ironmanId").addClass("ironmanClass"),
+    sequence:()=>{  $beamButton.appendTo($footer);
+    $missileButton.appendTo($footer);
+    $footer.appendTo($battlemap);}
+    
   },
   thor: {
     type: "hero",
     name: "thor",
     level: 1,
-    speed: 9,
+    spd: 9,
     hp: 140,
     atk: 15,
     atkgrowth: 8,
@@ -89,50 +94,51 @@ const characters = {
     type: "hero",
     name: "hulk",
     level: 1,
-    speed: 10,
+    spd: 10,
     hp: 200,
     atk: 15,
     atkgrowth: 6,
     hpgrowth: 35,
-  },
-  hand: {
-    type: "villain",
-    name: "hand",
-    level: 1,
-    speed: 9,
-    hp: 150,
-    atk: 15,
-    atkgrowth: 1,
-    hpgrowth: 1,
-    exp: 30,
-    pic: $("<img>")
-      .addClass("handClass")
-      .attr("id", "handId")
-      .attr("name", "hand"),
-  },
-  goon: {
-    type: "villain",
-    name: "goon",
-    level: 2,
-    speed: 8,
-    hp: 110,
-    atk: 25,
-    atkgrowth: 1,
-    hpgrowth: 1,
-    exp: 50,
-  },
-  thanos: {
-    type: "villain",
-    name: "thanos",
-    level: 5,
-    speed: 5,
-    hp: 1200,
-    atk: 50,
-    atkgrowth: 1,
-    hpgrowth: 1,
-    exp: 200,
-  },
+  }//,
+//   hand: {
+//     type: "villain",
+//     name: "hand",
+//     level: 1,
+//     speed: 9,
+//     hp: 150,
+//     atk: 15,
+//     atkgrowth: 1,
+//     hpgrowth: 1,
+//     exp: 30,
+//     pic: $("<img>")
+//       .addClass("handClass")
+//       .attr("id", "handId")
+//       .attr("name", "hand"),
+//   },
+//   goon: {
+//     type: "villain",
+//     name: "goon",
+//     level: 2,
+//     speed: 8,
+//     hp: 110,
+//     atk: 25,
+//     atkgrowth: 1,
+//     hpgrowth: 1,
+//     exp: 50,
+//   },
+//   thanos: {
+//     type: "villain",
+//     name: "thanos",
+//     level: 5,
+//     speed: 5,
+//     hp: 1200,
+//     atk: 50,
+//     atkgrowth: 1,
+//     hpgrowth: 1,
+//     exp: 200,
+//   },
 };
+
 for (const char in characters) {
   characters[char]["atk"] +=
     characters[char]["atkgrowth"] * characters[char]["level"];
@@ -159,6 +165,38 @@ const gameStart = () => {
 };
 
 // ### Creating Stages ####
+
+const loadStage1 =()=> {
+$(".battle").show("slow");
+let hero1 = "ironman" // from function
+let heroteam = null
+let villteam = null
+let spdArr =[]
+
+const charSet = {
+hand : { name:'hand', type: 'villain', Hp: 100, atk: 15, spd : 5, alive: true, skill: slash(), pic:$("<img>")
+.addClass("handClass")
+.attr("id", "handId")
+.attr("name", "hand")},
+
+hero1:{ name: hero[hero1].name, type: hero[hero1].type , Hp: hero[hero1].hp, atk: hero[hero1].atk, spd: hero[hero1].spd, alive: true, pic:hero[hero1].pic},
+}
+
+stageSetUp();
+
+for (let round = 1; round <= 5; round++) {
+for (const character of spdArr) {
+    if (charSet[character].type=='hero'){
+        hero[character].sequence()
+    }
+}
+}
+}
+
+
+
+
+
 const battleStage1 = () => {
   charArray = [];
   $(".battle").show("slow");
@@ -209,25 +247,52 @@ const ironmanAtkSequence = () => {
   $footer.appendTo($battlemap);
 };
 
-const skillButtons = () => {
-  $beamButton.on("click", funcAtkSelector);
-  $missileButton.on("click", funcAtkSelector);
-};
 
-const funcAtkSelector = (event) => {
-  atkselector = $(event.target).attr("name");
-  console.log(atkselector);
-  return atkselector;
-};
 
 // loadCharTurn();
 //  while (sumOf(heroHP)>0||sumOf(villHP)>0){}
 //determine which character turn to load
 //clear previous button & create new skill button in footer
 //listen for skill button click
-//select target, apply skill
+//select target, apply s
 
-const gameloop = () => {};
+
+
+//####################
+// GAME FUNCTIONS
+//####################
+
+const stageSetUp =()=>{
+    for (const x in charSet) {
+if (x.type === 'villain') {
+    villteam += x.hp
+}
+else { heroteam += x.hp}
+
+if(x.alive===true)
+SpdArr.push({[x.name] : [x.spd]})
+SpdArr.sort((a,b)=> b.spd-a.spd)
+}
+
+if (villteam < 1) { alert('win')};
+if(heroteam < 1 ) (alert('lose'));
+}
+
+//############################
+//Webpage onload Functions
+//############################
+
+const skillButtons = () => {
+    $beamButton.on("click", funcAtkSelector);
+    $missileButton.on("click", funcAtkSelector);
+  };
+  
+  const funcAtkSelector = (event) => {
+    atkselector = $(event.target).attr("name");
+    console.log(atkselector);
+    return atkselector;
+  };
+  
 
 const playSound = () => {
   document.getElementById("StartTheme").play();
